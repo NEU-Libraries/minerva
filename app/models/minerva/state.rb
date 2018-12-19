0e7dab26a25d07db2cdcf0d0ce73feb3924b4a72
David Cliff <d.cliff@neu.edu>
2018-12-19 11:09:08 -0500

    Adding in editing and deletion preventatives for the state model. To be truly auditable, it must stand as the system made them. No covering up person-made mistakes.

--- app/models/minerva/state.rb
+++ app/models/minerva/state.rb
@@ -2,5 +2,12 @@ module Minerva
   class State < ApplicationRecord
     belongs_to :assignment
     belongs_to :status
+    
+    before_destroy { |record| raise ReadOnlyRecord }
+    
+    def readonly?
+      # allow the creation, you can also use a more complex condition to allow updates under certain conditions
+      new_record? ? false : true 
+    end
   end
 end
