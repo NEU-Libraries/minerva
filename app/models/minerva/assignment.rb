1e36e41b8e76c7d5fa401f5f2a9998e9d34bb7c6
David Cliff <d.cliff@neu.edu>
2019-01-22 15:26:56 -0500

    Interfaces are optional for assignments

--- app/models/minerva/assignment.rb
+++ app/models/minerva/assignment.rb
@@ -1,5 +1,5 @@
 module Minerva
   class Assignment < ApplicationRecord
-    belongs_to :interface, :class_name => 'Minerva::Interface'
+    belongs_to :interface, :class_name => 'Minerva::Interface', optional: true
   end
 end
