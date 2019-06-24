fd08ad961fb8ce1f16cf6c007d7696915e18d8e0
David Cliff <dgcliff@northeastern.edu>
2019-06-24 16:02:11 -0400

    Migration fix, and model clean up

--- app/models/minerva/state.rb
+++ app/models/minerva/state.rb
@@ -1,19 +1,19 @@
 module Minerva
   class State < ApplicationRecord
-    
-    belongs_to :assignment, :class_name => 'Minerva::Assignment'
-    belongs_to :status, :class_name => 'Minerva::Status'
+
+    belongs_to :assignment
+    belongs_to :status
     belongs_to :role
     belongs_to :work
     belongs_to :user
-    
+
     belongs_to :creator, class_name: "Minerva::User"
-    
+
     before_destroy { |record| raise ActiveRecord::ReadOnlyRecord }
-    
+
     def readonly?
       # allow the creation, you can also use a more complex condition to allow updates under certain conditions
-      new_record? ? false : true 
+      new_record? ? false : true
     end
   end
 end
--- lib/minerva/version.rb
+++ lib/minerva/version.rb
@@ -1,3 +1,3 @@
 module Minerva
-  VERSION = '0.1.22'
+  VERSION = '0.1.23'
 end
